70c12f29c321c9d6e8b96be186981c513729049a
giovanni.tizzoni <giovanni.tizzoni150104@gmail.com>
2025-03-12 18:50:32 +0100

    Initial commit

---
+++ point.py
@@ -0,0 +1,62 @@
+import random
+
+class Point:
+    def __init__(self, x, y):
+        """
+        Initialise a point object
+        :param x: x-axis
+        :param y: y-axis
+        """
+        self.x = x # Define x attribute via self.x and assigns the value x to it
+        self.y = y # ""     y     ""        self.y
+
+    def __str__(self):
+        """
+        Magic method that is called when we try to print an instantiate
+        :return: [x, y]
+        """
+        return f"[{self.x}, {self.y}]"
+
+    def __repr__(self):
+        return self.__str__() # Use the same way of printing as 'str'
+
+    def distance_orig(self):
+        return (self.x**2 + self.y**2)**0.5  # Sqrt of the sum of x
+
+    def __gt__(self, other):
+        my_distance = self.distance_orig()
+        other_distance = other.distance_orig()
+        return my_distance > other_distance
+
+    def __eq__(self, other):
+        my_distance = self.distance_orig()
+        other_distance = other.distance_orig()
+        return my_distance > other_distance
+
+# Now we use points:
+p = Point(1, 2) # P is an instance of 1 and 2
+p_2 = Point(4.4, 50)
+
+print(f"p.x = {p.x} and p.y = {p.y}") # Prints the coordinates relative to what has be selected
+print(f"p_2.x = {p_2.x} and p_2.y = {p_2.y}")
+
+print(p)
+
+# Create a list of 5 random points
+points = []
+for i in range(5):
+    points.append(Point(random.randint(-10, 10), random.randint(-10, 10))) # First randint is for x while the second for y
+
+print("I got these 5 random points: ")
+for p in points:
+    print(p)
+
+p = Point(3, 4)
+print(p.distance_orig()) # Expect 5 answers
+
+p_2 = Point(1, 1)
+print(f"I am comparing p > p_2: {p>p_2}") # I expect to have true
+
+print("The sorted list of point is:")
+points.sort()
+print(points)
